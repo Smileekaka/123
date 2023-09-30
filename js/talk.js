8e3a1a2a0c2c241304ab411388e4a5ba5f42d021
Smileekaka <95348151+Smileekaka@users.noreply.github.com>
2023-09-30 23:32:03 +0800

    Update talk.js

--- js/talk.js
+++ js/talk.js
@@ -3,12 +3,12 @@ new TypeIt("#AndyPatty", {
         cursorSpeed: 1000,
         speed: 100
     })
-    .type("Andy && Patty")
+    .type("Andy <um style='color: #F44336;'>❤️</um> Patty")
     .pause(2000)
     .delete(null, {
         delay: 500
     })
-    .type("一起走過第三個520...")
+    .type("一起走過第四個生日...")
     .pause(3000)
     .go();
 
